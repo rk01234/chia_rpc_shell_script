7e369e5849f21f894ea60c5ce611d3d460df0dfc
rk01234 <52957642+rk01234@users.noreply.github.com>
2021-08-28 01:08:19 +0900

    test

--- RPC.sh
+++ RPC.sh
@@ -1,41 +1,62 @@
 #!/bin/bash
+# config/ssl folder
+dir=~/.chia/mainnet/config/ssl
 
-DIR=~/.chia
+# from config/config.yaml
+full_node_port=8555
+farmer_port=8559
+harvester_port=8560
+wallet_port=9256
 
-type=$1
-command=$2
-if [ $# -eq 2 ];then
-  data="{}"
-elif [ $# -lt 2 ];then
-  echo "Argument Error. example"
-  echo "./RPC.sh full_node get_wallet_balance"
-  echo "./RPC.sh wallet get_wallet_balance '{"wallet_id": 1}' "
-  echo "./RPC.sh wallet get_next_address '{"wallet_id": 1, "new_address":true}'"
-  type="go_else"
-else
-  data="${@:3}"
+while [ "$#" -ge "1" ]; do
+  if [ "$1" = "-h" ]; then
+    type="help"
+    break
+  elif [ "$1" = "-t" ] || [ "$1" = "--type" ];then
+    shift
+    type="$1"
+  elif [ "$1" = "-a" ] || [ "$1" = "--action" ];then
+    shift
+    action="$1"
+  elif [ "$1" = "-d" ] || [ "$1" = "--data" ];then
+    shift
+    data="$1"
+  fi
+  shift
+done
+
+if [ -z "$action" ] || [ -z "$type" ]; then
+  type="help"
 fi
 
 if [ ${type} = "full_node" ]; then
-  PORT=8555
+  port=${full_node_port}
 elif [ ${type} = "farmer" ]; then
-  PORT=8559
+  port=${farmer_port}
 elif [ ${type} = "harvester" ]; then
-  PORT=8560
+  port=${harvester_port}
 elif [ ${type} = "wallet" ]; then
-  PORT=9256
+  port=${wallet_port}
 else
-  echo "https://github.com/Chia-Network/chia-blockchain/wiki/RPC-Interfaces"
-  echo "https://github.com/Chia-Network/chia-blockchain/wiki/RPCExamples"
-  echo "https://github.com/rk01234/chia_rpc_shell_script"
-  echo "full_node"
-  echo "farmer"
-  echo "harvester"
-  echo "wallet"
-  exit 1
+    echo "https://github.com/Chia-Network/chia-blockchain/wiki/RPC-Interfaces"
+    echo "https://github.com/Chia-Network/chia-blockchain/wiki/RPCExamples"
+    echo "https://github.com/rk01234/chia_rpc_shell_script"
+    echo "-t --type   : [full_node,farmer,harverter,wallet]"
+    echo "-a --action : example get_wallet_balance"
+    echo "-d --data   : '{"wallet_id": 1, "new_address":true}'"
+    echo "               or not need."
+    echo "*====== example =====*"
+    echo "./rpc.sh -t full_node -a get_wallet_balance"
+    echo "./rpc.sh --type full_node --action get_wallet_balance"
+    echo ""
+    echo "./rpc.sh -t wallet -a get_wallet_balance -d '{"wallet_id": 1}' "
+    echo "./rpc.sh --type wallet --action get_wallet_balance --data '{"wallet_id": 1}' "
+    echo ""
+    echo "./rpc.sh -t wallet -a get_next_address -d '{"wallet_id": 1, "new_address":true}'"
+    exit 0
 fi
 
-curl -sS --insecure --cert ${DIR}/mainnet/config/ssl/${type}/private_${type}.crt \
- --key ${DIR}/mainnet/config/ssl/${type}/private_${type}.key \
+curl -sS --insecure --cert ${DIR}/${type}/private_${type}.crt \
+ --key ${DIR}/${type}/private_${type}.key \
  -d "${data}" -H "Content-Type: application/json" \
- -X POST https://localhost:${PORT}/${command} | python -m json.tool
+ -X POST https://127.0.0.1:${port}/${command} | python -m json.tool
